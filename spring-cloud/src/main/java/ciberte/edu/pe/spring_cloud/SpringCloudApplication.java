8469933b8efa904d7c4fafd06d3dde989cc78c5b
Juan Carlos <144704177+JuanCarlos1404@users.noreply.github.com>
2024-10-11 20:29:35 -0500

    eureka service

---
+++ spring-cloud/src/main/java/ciberte/edu/pe/spring_cloud/SpringCloudApplication.java
@@ -0,0 +1,13 @@
+package ciberte.edu.pe.spring_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudApplication.class, args);
+	}
+}
