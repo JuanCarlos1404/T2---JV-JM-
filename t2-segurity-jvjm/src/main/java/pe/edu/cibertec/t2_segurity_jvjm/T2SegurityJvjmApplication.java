1d1bbe2727aa37164dbb700cbfe88b9f3a771cef
27jorg16 <146002952+27jorg16@users.noreply.github.com>
2024-10-11 19:29:56 -0500

    inicio

---
+++ t2-segurity-jvjm/src/main/java/pe/edu/cibertec/t2_segurity_jvjm/T2SegurityJvjmApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t2_segurity_jvjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2SegurityJvjmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2SegurityJvjmApplication.class, args);
+	}
+
+}
---
+++ t2-segurity-jvjm/src/test/java/pe/edu/cibertec/t2_segurity_jvjm/T2SegurityJvjmApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.t2_segurity_jvjm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T2SegurityJvjmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
